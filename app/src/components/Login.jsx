984d24d2dc861b2a87f488f95f2165d9e8b67997
krikaz <pascal.bou@protonmail.com>
2019-10-21 16:58:50 +0200

    comp register, login

---
+++ app/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+	return (
+		<StyledContainer>
+				<StyledInput type="text" placeholder="username" />
+
+				<StyledInput type="password" placeholder="password" />
+
+			<StyledButton type="submit" >
+				Log In
+			</StyledButton>
+		</StyledContainer>
+	);
+}
+
+const StyledContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	width: auto;
+	background-color: rgb(255, 183, 82, 0.9);
+	color: rgb(3, 84, 16);
+`;
+
+const StyledInput = styled.input`
+	font-size: 1.5rem;
+	margin: 0.5rem;
+`;
+
+const StyledButton = styled.button`
+	font-size: 1.5rem;
+	margin: 0.5rem;
+`;
